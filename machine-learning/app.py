af4ca60ee27270b118cdb780fb30939a949494fe
Dea Luthfina Azzahra <122023232+deafiana@users.noreply.github.com>
2025-05-25 21:01:13 +0700

    Update app.py
    port

--- machine-learning/app.py
+++ machine-learning/app.py
@@ -169,7 +169,8 @@ def predict():
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))  # fallback for local
+    app.run(host='0.0.0.0', port=port)
 
 # from pyngrok import ngrok
 # if __name__ == "__main__":
