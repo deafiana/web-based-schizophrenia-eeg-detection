a5dec852413c13b9fad5c1134d97f06069b166f6
deafiana <dea.luthfina.azzahra.tik21@mhsw.pnj.ac.id>
2025-05-25 22:08:49 +0700

    app.py port

--- machine-learning/app.py
+++ machine-learning/app.py
@@ -169,8 +169,9 @@ def predict():
 
     return response
 #
-# if __name__ == "__main__":
-    # app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
 
 # from pyngrok import ngrok
 # if __name__ == "__main__":
